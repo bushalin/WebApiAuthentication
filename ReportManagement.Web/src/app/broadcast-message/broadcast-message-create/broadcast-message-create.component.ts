19d169fd1c710eb8ac449ed5406b881f45790096
Imranul Islam <shoeb1219@gmail.com>
2020-03-30 11:31:49 +0900

    message create modal

--- ReportManagement.Web/src/app/broadcast-message/broadcast-message-create/broadcast-message-create.component.ts
+++ ReportManagement.Web/src/app/broadcast-message/broadcast-message-create/broadcast-message-create.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BroadcastMessageCreateModel } from 'src/models/broadcastMessage';
 import { AuthenticationService } from 'src/services/authentication.service';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-broadcast-message-create',
@@ -11,13 +13,22 @@ import { AuthenticationService } from 'src/services/authentication.service';
 export class BroadcastMessageCreateComponent implements OnInit {
 
   messageCreateForm: FormGroup;
-  loading = false;
   submitted = false;
   userDataSubscription;
   userData;
+  messageFormData: any = {};
+
+  // ngx-modal configuration and implementation
+  modalRef: BsModalRef;
+  modalConfig = {
+    backdrop: true,
+    class: "modal-xl"
+  };
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private router: Router,
+    private modalService: BsModalService,
   ) {
     // getting user data
     this.userDataSubscription = this.authService.userData
@@ -38,17 +49,48 @@ export class BroadcastMessageCreateComponent implements OnInit {
     return this.messageCreateForm.controls;
   }
 
-  onSubmit() {
+  onSubmit(template: TemplateRef<any>) {
     this.submitted = true;
     if (this.messageCreateForm.invalid) {
       return;
     }
     let messageModel = new BroadcastMessageCreateModel();
+    this.messageFormData.messageTitle = this.messageCreateForm.controls["messageTitle"].value;
+    this.messageFormData.messageBody = this.messageCreateForm.controls["messageBody"].value;
     messageModel.UserId = this.userData.userId;
     messageModel.MessageTitle = this.messageCreateForm.controls["messageTitle"].value;
     messageModel.MessageBody = this.messageCreateForm.controls["messageBody"].value;
-    this.loading = true;
-    console.log(messageModel);
+    this.modalRef = this.modalService.show(template, this.modalConfig);
+    console.log(this.messageFormData);
+    
+  }
+
+  modalConfirm() {
+    // calling api to save data
+    // this.reportService.saveReport(this.reportFormData).subscribe(
+    //   data => {
+    //     console.log(data);
+        
+    //   },
+    //   error => {
+        
+    //   }
+    // );
+    console.log(this.messageFormData);
+    this.modalRef.hide();
+
+    this.router.navigateByUrl("/", { skipLocationChange: true }).then(() => {
+      this.router.navigate(["/broadcast"]);
+    });
+  }
+
+  modalDecline() {
+    this.submitted = false;
+    if (this.messageCreateForm.invalid) {
+      return;
+    }
+
+    this.modalRef.hide();
   }
 
 }
